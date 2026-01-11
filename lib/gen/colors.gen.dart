bda643e1219e3e517e5ddc73f629d513c4868f5a
Super Abel <superabel000@gmail.com>
2026-01-11 02:43:39 +0100

    Add colors configuration

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,28 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+
+class AppColors {
+  AppColors._();
+
+  /// Color: #2196F3
+  static const Color main = Color(0xFF2196F3);
+
+  /// Color: #F5F7FA
+  static const Color background = Color(0xFFF5F7FA);
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+
+  /// Color: #000000
+  static const Color black = Color(0xFF000000);
+}
