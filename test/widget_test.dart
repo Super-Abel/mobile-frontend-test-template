3e0bf05d5aa6e0bfa55745870f5c7cb492f08dc2
Super Abel <superabel000@gmail.com>
2026-01-11 03:46:42 +0100

    Fix widget smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_dashboard/main.dart';
 
 void main() {
   testWidgets('Dashboard app smoke test', (WidgetTester tester) async {
+    tester.view.physicalSize = const Size(800, 1200);
+    tester.view.devicePixelRatio = 1.0;
+
     await tester.pumpWidget(const MyApp());
-    await tester.pumpAndSettle();
+    await tester.pumpAndSettle(const Duration(seconds: 3));
+
     expect(find.text('Sales'), findsWidgets);
+
+    addTearDown(() {
+      tester.view.resetPhysicalSize();
+      tester.view.resetDevicePixelRatio();
+    });
   });
 }
